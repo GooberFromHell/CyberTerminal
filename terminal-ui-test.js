3d714fe0945808e5432f526514aadc83c85a1d30
GooberFromHell <wcottrell01@gmail.com>
2023-11-05 18:47:47 -0600

    Merged from dev

--- terminal-ui-test.js
+++ terminal-ui-test.js
@@ -1,27 +1,76 @@
-const reworkContainer = $('#rework-container')
-const vmInterface = $('#vmware-interface')
-const terminalContainer = $('#terminal')
-const divider = $('#divider')
-const terminalTools = $('#terminal-tools')
-const uploadOverlay = $('#upload-overlay')
+// ==UserScript==
+// @name         PCTE Term UI Test
+// @version      4.2
+// @author       @LordGoober
+// @match        http://127.0.0.1:5500/index.html
+// @require      https://code.jquery.com/jquery-3.7.1.js
+// @require      https://code.jquery.com/ui/1.13.2/jquery-ui.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/2.37.2/js/jquery.terminal.js
+// @resource     jqterminal_css https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/2.37.2/css/jquery.terminal.css
+// @resource     jquery_ui_css https://code.jquery.com/ui/1.13.2/themes/ui-darkness/jquery-ui.css
+// @resource     fontawesome_css https://raw.githubusercontent.com/bryfry/chronos_trigger/master/font-awesome.css
+// @resource     hack_font https://raw.githubusercontent.com/GooberFromHell/term/master/hack.css
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @run-at       document-end
+// @sandbox      MAIN_PAGE
+// @require      file://D:/Code/js/Term 4.0/terminal-ui-test.js
+// ==/UserScript==
 
-reworkContainer.css({
-    position: "absolute",
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
+const jqueryui_css = getResourceText("jqueryui_css")
+const jquerys_css = getResourceText("jquerys_css")
+const juqeryt_css = getResourceText("juqeryt_css")
+const fontawesome_css = getResourceText("fontawesome_css")
+const hack_css = getResourceText("hack_css")
+const terminal_css = getResourceText("terminal_css")
+
+addStyle(jqueryui_css)
+addStyle(jquerys_css)
+addStyle(juqeryt_css)
+addStyle(fontawesome_css)
+addStyle(hack_css)
+addStyle(terminal_css)
+
+const $ = jQuery
+var wmks = undefined
+
+const rebase_css = {
+    height: '100vh',
+    width: '100vw',
+    margin: '0px',
+    padding: '0px',
+}
+
+const style = {
+    flex: {
+        display: 'flex',
+    },
+    flexCenter: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    flexColumn: {
+        flexDirection: 'column',
+    },
+    flexCenterColumn: {
+        ...this.flexCenter,
+        ...this.flexColumn,
+    }
+}
+
+const reworkContainerStyles = {
+    minHeight: '100%',
     display: 'flex',
     'flex-direction': 'column',
     'font-family': 'Hack, "Segoe UI" !important'
 })
-vmInterface.css({
-    position: 'fixed',
-    flex: '1 1 auto',
-    height: '80%',
-    width: '100%',
-    'background-color': 'black',
-})
+    vmInterface.css({
+        position: 'fixed',
+        flex: '1 1 auto',
+        height: '80%',
+        width: '100%',
+        'background-color': 'black',
+    })
 terminalContainer.css({
     position: 'fixed',
     bottom: 0,
